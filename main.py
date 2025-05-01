1898e6236bd032c508c4aaf71ac90ae1294e774a
samwilliam2025 <wil-samuel08@hotmail.com>
2025-05-01 20:12:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi import Form, File, UploadFile
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "API está funcionando!"}
+
 @app.post("/upload-image")
 async def upload_image(file: UploadFile = File(...)):
-    # Aqui você pode adicionar a lógica para processar a imagem, como comparar com um banco de dados ou fazer verificação.
-    # Por enquanto, vamos simular um resultado de imagem encontrada com uma URL fictícia.
-
-    result = {
-        "status": "encontrada",
-        "message": "Imagem encontrada!",
-        "image_url": "https://example.com/imagem-falsa.jpg"  # Exemplo de URL de imagem
-    }
-
-    return JSONResponse(content=result)
+    return JSONResponse(content={"filename": file.filename})
